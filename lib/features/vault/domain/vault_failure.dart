7b4514af907a83681cbc7623ba67ab3be7b82cb8
yappachino <89217414+mad-yappachino@users.noreply.github.com>
2026-04-16 19:08:41 +0300

    feat(vault): add VaultFailure

--- lib/features/vault/domain/vault_failure.dart
+++ lib/features/vault/domain/vault_failure.dart
@@ -1,11 +1,14 @@
-sealed class VaultException implements Exception {
-  const VaultException();
-}
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:frontend/core/domain/core_failure.dart';
 
-class VaultNotInitializedException extends VaultException {
-  const VaultNotInitializedException();
-}
+part 'vault_failure.freezed.dart';
 
-class InvalidPinException extends VaultException {
-  const InvalidPinException();
+@freezed
+sealed class VaultFailure with _$VaultFailure {
+  const factory VaultFailure.vaultLocked() = _VaultLocked;
+  const factory VaultFailure.vaultNotInitialized() = _VaultNotInitialized;
+  const factory VaultFailure.invalidSecret() = _InvalidSecret;
+
+  const factory VaultFailure.core(CoreFailure core) = _VaultCore;
 }
