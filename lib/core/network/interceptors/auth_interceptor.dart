16158a4bcd96e20d131c0ce9e080ede83aa411a1
yappachino <89217414+mad-yappachino@users.noreply.github.com>
2026-04-24 15:16:26 +0300

    feat(network): implement automatic token refresh

--- lib/core/network/interceptors/auth_interceptor.dart
+++ lib/core/network/interceptors/auth_interceptor.dart
@@ -1,19 +1,65 @@
 import 'package:dio/dio.dart';
-import 'package:frontend/core/session/auth_session.dart';
+import 'package:frontend/core/network/dio_provider.dart';
+import 'package:frontend/core/session/session_manager.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 class AuthInterceptor extends Interceptor {
-  AuthInterceptor(this._authSession);
+  AuthInterceptor(this._ref);
 
-  final AuthSession _authSession;
+  final Ref _ref;
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    final token = _authSession.accessToken;
+    final token = _ref.read(authSessionProvider).accessToken;
 
     if (token != null) {
       options.headers['Authorization'] = 'Bearer $token';
     }
 
     handler.next(options);
+  }
+
+  @override
+  Future<void> onError(
+    DioException err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    if (err.response?.statusCode == 401) {
+      final refreshResult = await _ref
+          .read(tokenRefresherProvider)
+          .refreshToken();
+
+      return refreshResult.fold(
+        (token) async {
+          _ref.read(sessionControllerProvider).setAuthToken(token);
+
+          final opts = Options(
+            method: err.requestOptions.method,
+            headers: err.requestOptions.headers,
+          );
+
+          opts.headers?['Authorization'] = 'Bearer $token';
+
+          try {
+            final refreshDio = _ref.read(refreshDioProvider);
+
+            final retryResponse = await refreshDio
+                .request<Map<String, dynamic>>(
+                  err.requestOptions.path,
+                  options: opts,
+                  data: err.requestOptions.data,
+                  queryParameters: err.requestOptions.queryParameters,
+                );
+
+            return handler.resolve(retryResponse);
+          } on DioException catch (retryErr) {
+            return handler.reject(retryErr);
+          }
+        },
+        (failure) => handler.reject(err),
+      );
+    }
+
+    handler.next(err);
   }
 }
